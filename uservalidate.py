87bec404e7488fcddd38b4f82159171754c716d1
Swapnil Dhoble <78730160+swapnildhoble@users.noreply.github.com>
2021-08-13 23:04:23 +0530

    Delete uservalidate.py

--- uservalidate.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, IntegerField, TextAreaField, SubmitField, SelectField
-from wtforms import validators, ValidationError
-
-class saveuserdata(Form):
-    email = TextField("email", [validators.Required("Please enter your email address."),
-                                validators.Email("Please enter your email address.")])
